443ddba1b539b90a77a73fea450bff55b8131614
schhetri1208 <schhetri1208@gmail.com>
2025-05-16 00:50:19 -0400

    Change the url end point to local

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -11,7 +11,7 @@ function LoginPage() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://13.59.54.29:8080/api/auth/login', {
+      const response = await axios.post('http://localhost:8080/api/auth/login', {
         email,
         password
       });
